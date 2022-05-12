d9610db0734bb2dd934d4535e2054d7caa36f8fb
BloodyRanger <bloodyranger@inbox,ru>
2022-05-12 11:27:49 +0300

    Final variant

--- src/test/java/geo/GeoServiceImplTest.java
+++ src/test/java/geo/GeoServiceImplTest.java
@@ -1,17 +1,17 @@
 package geo;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoServiceImpl;
 
-import java.util.Locale;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GeoServiceImplTest {
 
@@ -29,5 +29,11 @@ public class GeoServiceImplTest {
                 Arguments.of(GeoServiceImpl.NEW_YORK_IP, Country.USA));
     }
 
-
+    @Test
+    public void testByCoordinates() {
+        double a = 1.0, b = 1.0;
+        var expected = RuntimeException.class;
+        assertThrows(expected,
+                () -> new GeoServiceImpl().byCoordinates(a, b));
+    }
 }
